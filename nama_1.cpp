3ec90bd446ca20e3b8e66c02d22167d95f9914f5
Bagasramdanfauzi <111625202+Bagasramdanfauzi@users.noreply.github.com>
2022-09-19 15:56:09 +0700

    https://github.com/Bagasramdanfauzi/rpl2-.git

---
+++ nama_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout <<"nama guntur"<< endl;
+    cout <<" kelas xrpl 2"<< endl;
+    cout <<"85"<< endl;
+    return 0;
+}
